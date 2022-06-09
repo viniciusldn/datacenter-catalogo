f29fd08d4a661f90872301dbd695a4bd69e078c1
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-09 10:52:27 -0300

    :sparkles: Entidade Aplicacao

--- src/main/java/com/trybe/acc/java/datacenter/entity/Aplicacao.java
+++ src/main/java/com/trybe/acc/java/datacenter/entity/Aplicacao.java
@@ -1,6 +1,54 @@
 package com.trybe.acc.java.datacenter.entity;
 
-public class Aplicacao {
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Aplicacao {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String nome;
+  private String descricao;
+
+  @ManyToMany(mappedBy = "Aplicacao")
+  private Set<Servidor> servidores;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Set<Servidor> getServidores() {
+    return servidores;
+  }
+
+  public void setServidores(Set<Servidor> servidores) {
+    this.servidores = servidores;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
 
 }
