b338dff6f4a0144f3d1154ac9a16ab6a42fc3e62
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-09 11:21:04 -0300

    :sparkles: Servico Aplicacao

--- src/main/java/com/trybe/acc/java/datacenter/service/AplicacaoService.java
+++ src/main/java/com/trybe/acc/java/datacenter/service/AplicacaoService.java
@@ -1,7 +1,49 @@
-package com.trybe.acc.java.datacenter.service;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import com.trybe.acc.java.datacenter.entity.Aplicacao;
 
 public class AplicacaoService implements ServiceInterface<Aplicacao, Long> {
 
+  public void save(Aplicacao app) {
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    em.persist(app);
+    em.getTransaction().commit();
+    em.close();
+  }
+
+  public void update(Aplicacao app) {
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    em.merge(app);
+    em.getTransaction().commit();
+    em.close();
+  }
+
+  public void delete(Long id) {
+    EntityManager em = emf.createEntityManager();
+
+    Aplicacao app = em.find(Aplicacao.class, id);
+
+    em.getTransaction().begin();
+    em.remove(app);
+    em.getTransaction().commit();
+    em.close();
+  }
+
+  public List<Aplicacao> list() {
+    EntityManager em = emf.createEntityManager();
+
+    TypedQuery<Aplicacao> query = em.createQuery("from Aplicacao", Aplicacao.class);
+    List<Aplicacao> lstApp = query.getResultList();
+    return lstApp;
+  }
+
+  public Aplicacao findById(Long id) {
+    EntityManager em = emf.createEntityManager();
+    return em.find(Aplicacao.class, id);
+  }
 }
